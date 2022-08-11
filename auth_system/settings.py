0e2a6ed3a45c7e010db8f17c02bcf85ea0d7937d
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2022-08-11 22:39:38 +0530

    edited 3

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
 
     'allauth',
     'allauth.account',
